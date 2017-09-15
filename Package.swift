c22ba4f27c30d533956b63ab34ed04365a7bee44
Adam Nemecek <adamnemecek@gmail.com>
2017-09-15 13:51:04 -0700

    added version requirement to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBitset",
+    swiftLanguageVersions: [4],
     targets: [
         Target(name: "Bitset", dependencies: [])
     ]
