c091ae45ed34acf39e435a898222517454114e54
Daniel Lemire <lemire@gmail.com>
2017-11-21 17:54:12 -0500

    Adding a product name

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBitset",
+    products: [
+        .library(name: "Bitset", targets: ["Bitset"]),
+    ],
     targets: [
         .target(name: "Bitset", dependencies: []),
         .testTarget(name: "BitsetTests", dependencies: ["Bitset"])
